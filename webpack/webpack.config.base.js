003e4b1dddabfca4476a431c65e9d6e6cea3ce95
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-02-07 19:45:47 +1100

    build: remove hardsource plugin as its bugs

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -107,17 +106,6 @@ const baseConfig = {
       filename: 'css/[name].css',
       chunkFilename: 'css/[id].css',
     }),
-    new HardSourceWebpackPlugin(),
-    new HardSourceWebpackPlugin.ExcludeModulePlugin([
-      {
-        // HardSource works with mini-css-extract-plugin but due to how
-        // mini-css emits assets, assets are not emitted on repeated builds with
-        // mini-css and hard-source together. Ignoring the mini-css loader
-        // modules, but not the other css loader modules, excludes the modules
-        // that mini-css needs rebuilt to output assets every time.
-        test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
-      },
-    ]),
   ],
 };
 
